a66f21fc941beb1f30e9e09b3d2b215903985287
Vanilson <vanilsonvl@gmail.com>
2022-11-28 22:34:42 -0300

    202

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { SafeAreaView, View, Text } from 'react-native';
+import FormLogin from './components/formLogin';
+
+export default props => (
+  <FormLogin />
+)
